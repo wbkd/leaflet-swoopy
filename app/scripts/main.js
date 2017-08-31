5db95557e506af227fcf2aebf053d53ccb1f3c70
Stefan Keil <an.stefan.keil@googlemail.com>
2017-08-31 13:07:36 +0200

    add arrow, remove swoopyarrowjs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,15 @@
-
-const swoopyArrowHelper = require('./swoopyArrow').default;
+// const swoopyArrowHelper = require('./swoopyArrow').default;
 const TurfBezier = require('turf-bezier');
 const L = require('leaflet');
 const curve = require('leaflet-curve');
 
 
-const map = L.map('map').setView([52.52, 13.4], 9);
+const map = L.map('map', {
+  renderer: L.svg()
+}).setView([52.52, 13.4], 9);
 
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.SwoopyArrow = L.Layer.extend({
@@ -17,7 +18,7 @@ L.SwoopyArrow = L.Layer.extend({
     toLatlng: [],
   },
 
-  initialize: function(options) {
+  initialize: function (options) {
     L.Util.setOptions(this, options);
 
     this._fromLatlng = L.latLng(this.options.fromLatlng);
@@ -26,54 +27,91 @@ L.SwoopyArrow = L.Layer.extend({
     this._initSVG();
   },
 
-  _initSVG: function() {
+  _initSVG: function () {
     this._svg = L.SVG.create('svg');
-    this._path = L.SVG.create('path');
 
-    L.DomUtil.addClass(this._svg, 'leaflet-swoopyarrow');
-    L.DomUtil.addClass(this._path, 'leaflet-swoopyarrow__path');
-
-    this._path.setAttribute('stroke', '#000');
-    this._path.setAttribute('fill', 'none');
-    this._path.setAttribute('marker-end', 'url(#arrowhead)');
-
-    this._svg.appendChild(this._path);
+    this._arrow = this._createArrow();
+    this._svg.appendChild(this._arrow);
   },
 
-  onAdd: function(map) {
+  onAdd: function (map) {
     this._map = map;
     this.getPane().appendChild(this._svg);
 
     this.update();
   },
 
-  _getSwoopyPath: function(map) {
-    return swoopyArrowHelper()
-      .angle(Math.PI/4)
-      .x(d => this._map.latLngToLayerPoint([d.lat, d.lng]).x)
-      .y(d => this._map.latLngToLayerPoint([d.lat, d.lng]).y);
-  },
 
   getEvents: function () {
-		return {
-			zoom: this.update,
-			viewreset: this.update
-		};
-	},
+    return {
+      zoom: this.update,
+      viewreset: this.update
+    };
+  },
 
-  update: function() {
-    const swoopyPath = this._getSwoopyPath()([this._fromLatlng,this._toLatlng]);
-    this._path.setAttribute('d', swoopyPath);
+  _createArrow: function () {
+    const container = L.SVG.create('defs');
+    const marker = L.SVG.create('marker');
+    const path = L.SVG.create('polyline');
 
-    var pos = this._map.latLngToLayerPoint(this._fromLatlng).round();
+    L.DomUtil.addClass(marker, 'leaflet-swoopyarrow');
+    L.DomUtil.addClass(path, 'leaflet-swoopyarrow__path');
 
-    console.log(pos)
+    marker.setAttribute('id', 'swoopyarrow__arrowhead');
+    marker.setAttribute('markerWidth', '20');
+    marker.setAttribute('markerHeight', '20');
+    marker.setAttribute('viewBox', '-10 -10 20 20');
+    marker.setAttribute('orient', 'auto');
+    marker.setAttribute('refX', '0');
+    marker.setAttribute('refY', '0');
+    marker.setAttribute('fill', 'none');
+    marker.setAttribute('stroke', 'red');
+    marker.setAttribute('stroke-width', '1');
 
-    const pathBBox = this._path.getBBox();
+    path.setAttribute('stroke-linejoin', 'bevel');
+    path.setAttribute('fill', 'none');
+    path.setAttribute('stroke', 'red');
+    path.setAttribute('points', '-6.75,-6.75 0,0 -6.75,6.75');
 
- //   L.DomUtil.setPosition(this._svg, L.point(pathBBox))
-    this._svg.setAttribute('width', window.innerWidth);
-    this._svg.setAttribute('height', window.innerHeight);
+    marker.appendChild(path);
+    container.appendChild(marker);
+
+    return container;
+  },
+
+  _createPath: function () {
+    //quadratic bezier curve
+    const curvePoint = this._calcBezierControlPoints(this._fromLatlng, this._toLatlng, 8);
+
+    const pathOne = L.curve(
+      [
+        'M', [this._fromLatlng.lat, this._fromLatlng.lng],
+        'Q', curvePoint, [this._toLatlng.lat, this._toLatlng.lng]
+
+      ], {
+        animate: false,
+        color: 'red',
+        fill: false,
+        weight: 1,
+        className: 'pathone'
+      }
+    ).addTo(map);
+
+    const pathNode = document.querySelector('.pathone');
+    pathNode.setAttribute('marker-end', 'url(#swoopyarrow__arrowhead)');
+
+    return pathNode;
+  },
+
+  _calcBezierControlPoints: function (firstPoint, lastPoint, factor) {
+    const xDiff = Math.abs(lastPoint.lng - firstPoint.lng);
+    const x1 = firstPoint.lng + xDiff / 2;
+    const y1 = lastPoint.lat + xDiff / factor;
+    return ([y1, x1]);
+  },
+
+  update: function () {
+    const swoopyPath = this._createPath();
 
     return this;
   }
@@ -83,28 +121,7 @@ function swoopyArrow(options) {
   return new L.SwoopyArrow(options);
 }
 
-//  swoopyArrow({
-//    fromLatlng: [52.52, 13.4],
-//    toLatlng: [52.525, 14.405]
-//  }).addTo(map)
-
-var calcBezierControlPoints = function(firstPoint, lastPoint, factor) {
-    var xDiff = Math.abs(lastPoint.x - firstPoint.x);
-    var x1 = firstPoint.x + xDiff / 2;
-    var y1 = lastPoint.y + xDiff / factor;
-    return ([y1, x1]);
-}
-
-//quadratic bezier curve
-var curvePoint = calcBezierControlPoints({y: 52.4, x: 13.4}, {y:52.525, x:14.405}, 8);
-
-var pathOne = L.curve(
-  [
-   'M', [52.4, 13.4],
-   'Q', curvePoint,
-       [52.525, 14.405]
-  ], {animate: false, color:'red', fill:false}
-).addTo(map);
-                
-
-             
+swoopyArrow({
+  fromLatlng: [52.52, 13.4],
+  toLatlng: [52.525, 14.405]
+}).addTo(map)
